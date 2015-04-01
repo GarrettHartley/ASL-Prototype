059372b70c73c95ce56f0476428693f3ed419348
Kara Hartley <karahartley@karas-mbp-3.app.byu.edu>
2015-04-01 14:07:37 -0600

    Initial commit

---
+++ objects.js
@@ -0,0 +1,51 @@
+var dict = {
+	Blue: {url: 'url/ant', Display: 'Blue', unit: '1'},
+	Hello: {url:'url/Bug', Display: 'Hello', unit: '2'},
+	Which: {url:'url/cat', Display: 'Which', unit: '3'},
+};
+
+var current_user = 'Joe';
+var logged_in = false;
+
+var user_words = {
+	Blue: {url:'url/Bug', Display: 'Blue', username: 'Joe'},
+	Hello: {url:'url/cat', Display: 'Hello', username: 'Joe'},
+	Which: {url: 'url/ant', Display: 'Which', username: 'Joe'},
+};
+
+var finger_spell = {
+	A: '/letter_images/A.GIF',
+	B: '/letter_images/B.GIF',
+	C: '/letter_images/C.GIF',
+	D: '/letter_images/D.GIF',
+	E: '/letter_images/E.GIF',
+	F: '/letter_images/F.GIF',
+	G: '/letter_images/G.GIF',
+	H: '/letter_images/H.GIF',
+	I: '/letter_images/I.GIF',
+	J: '/letter_images/J.GIF',
+	K: '/letter_images/K.GIF',
+	L: '/letter_images/L.GIF',
+	M: '/letter_images/M.GIF',
+	N: '/letter_images/N.GIF',
+	O: '/letter_images/O.GIF',
+	P: '/letter_images/P.GIF',
+	Q: '/letter_images/Q.GIF',
+	R: '/letter_images/R.GIF',
+	S: '/letter_images/S.GIF',
+	T: '/letter_images/T.GIF',
+	U: '/letter_images/U.GIF',
+	V: '/letter_images/V.GIF',
+	W: '/letter_images/W.GIF',
+	X: '/letter_images/X.GIF',
+	Y: '/letter_images/Y.GIF',
+	Z: '/letter_images/Z.GIF'
+};
+
+
+var requested_words= {
+	//EXAMPLE REQUESTS
+	Chair: {word: 'chair', num_requests: 8},
+	Giraffe: {word: 'giraffe', num_requests: 3},
+	Pineapple: {word: 'pineapple', num_requests: 2}
+};
